98415a89309f5904d2e6ba90a720c0e99e5ac21b
Jeeee <a.zubaidd@gmail.com>
2022-06-03 10:00:18 +0700

    add sum shit 1

--- main.py
+++ main.py
@@ -9,18 +9,18 @@ def main():
     data = csv_file[["X.1", "X.2"]]
     data_l = data.values.tolist();
 
-    URL_PATTERN = r'[A-Za-z0-9]+://[A-Za-z0-9%-_]+(/[A-Za-z0-9%-_])*(#|\\?)[A-Za-z0-9%-_&=]*'
-    regx = re.compile(URL_PATTERN);
-
-    i = 1
-    for col in data_l:
-        print(i, end='. ')
-        print(col[1])
-        i += 1
+    # i = 1
+    # for col in data_l:
+    #     print(i, end='. ')
+    #     print(col[1])
+    #     i += 1
 
     print('='*100)
 
     # remove any link
+    URL_PATTERN = r'[A-Za-z0-9]+://[A-Za-z0-9%-_]+(/[A-Za-z0-9%-_])*(#|\\?)[A-Za-z0-9%-_&=]*'
+    regx = re.compile(URL_PATTERN)
+
     for data in data_l:
         if regx.search(data[0]):
             data[0] = re.sub(URL_PATTERN, '', data[0])
@@ -37,12 +37,20 @@ def main():
         data[1] = to_gmt7(data[1])
 
     # get data that contain trafic information
+    TRAFFIC_PATTERN = r"[0-9]+\.+[0-9]"
+    regx = re.compile(TRAFFIC_PATTERN)
 
     i = 1
-    for col in data_l:
-        print(i, end='. ')
-        print(col)
-        i += 1
+    for data in data_l:
+        if "arus" in data[0]:
+            print(data[0])
+        i+=1
+
+    # i = 1
+    # for col in data_l:
+    #     print(i, end='. ')
+    #     print(col)
+    #     i += 1
 
 
 def to_gmt7(twitter_date):
