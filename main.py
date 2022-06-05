ca328fa3ea2d3326eeaa721e04d5830e53942595
Jeeee <a.zubaidd@gmail.com>
2022-06-05 21:50:34 +0700

    add sum shit 2

--- main.py
+++ main.py
@@ -1,56 +1,68 @@
+import pandas
 import pandas as pd
-import time
 import re
-import time
 from datetime import datetime, timedelta
 
 def main():
-    csv_file = pd.read_csv('TwitterPoldaMetroJaya.csv')
-    data = csv_file[["X.1", "X.2"]]
-    data_l = data.values.tolist();
+    filtered_data = []
+    f_name = ['poldametro31.csv', 'poldametro01.csv', 'poldametro02.csv', 'poldametro03.csv']
 
-    # i = 1
-    # for col in data_l:
-    #     print(i, end='. ')
-    #     print(col[1])
-    #     i += 1
+    for filename in f_name:
+        csv_file = pd.read_csv(filename)
+        data = csv_file[["Content", "Date"]]
+        data_l = data.values.tolist();
+        temp_filtered_data = []
 
-    print('='*100)
+        # Select data that contain "arus" and "lintas"
+        for dl in data_l:
+            if "arus" in dl[0] and "lintas" in dl[0]:
+                temp_filtered_data.append([dl[0], dl[1]])
 
-    # remove any link
-    URL_PATTERN = r'[A-Za-z0-9]+://[A-Za-z0-9%-_]+(/[A-Za-z0-9%-_])*(#|\\?)[A-Za-z0-9%-_&=]*'
-    regx = re.compile(URL_PATTERN)
+        # Remove link from text
+        URL_PATTERN = r'[A-Za-z0-9]+://[A-Za-z0-9%-_]+(/[A-Za-z0-9%-_])*(#|\\?)[A-Za-z0-9%-_&=]*'
+        regx = re.compile(URL_PATTERN)
 
-    for data in data_l:
-        if regx.search(data[0]):
-            data[0] = re.sub(URL_PATTERN, '', data[0])
+        for tfd in temp_filtered_data:
+            if regx.search(tfd[0]):
+                tfd[0] = re.sub(URL_PATTERN, '', tfd[0])
 
-    # remove empty list
-    n = 0
-    for data in data_l:
-        if not data[0]:
-            data_l.pop(n)
-        n += 1
+        # Remove empty array
+        n = 0
+        for tfd in temp_filtered_data:
+            if not tfd[0]:
+                temp_filtered_data.pop(n)
+            n += 1
 
-    # convert twitter timestamp to gmt +7
-    for data in data_l:
-        data[1] = to_gmt7(data[1])
+        # Convert Twitter Timestamp to GMT+7
+        for tfd in temp_filtered_data:
+            tfd[1] = to_gmt7(tfd[1])
 
-    # get data that contain trafic information
-    TRAFFIC_PATTERN = r"[0-9]+\.+[0-9]"
-    regx = re.compile(TRAFFIC_PATTERN)
+        #
+        singkatan = [
+            ["Jl.", "Jalan"],
+            ["TL", "Traffic Light"],
+            ["Tl", "Traffic Light"],
+            ["Jaksel", "Jakarta Selatan"],
+            ["Jakut", "Jakarta Utara"],
+            ["Jakbar", "Jakarta Barat"],
+            ["Jaktim", "Jakarta Timur"],
+            ["Jakpus", "Jakarta Pusat"]
+        ]
 
-    i = 1
-    for data in data_l:
-        if "arus" in data[0]:
-            print(data[0])
-        i+=1
+        n = 0
+        for tfd in temp_filtered_data:
+            for s in singkatan:
+                if s[0] in tfd[0]:
+                    str = temp_filtered_data[n][0]
+                    str = str.replace(s[0], s[1])
+                    temp_filtered_data[n][0] = str
+            n += 1
 
-    # i = 1
-    # for col in data_l:
-    #     print(i, end='. ')
-    #     print(col)
-    #     i += 1
+        for fd in temp_filtered_data:
+            filtered_data.append(fd)
+
+    df = pandas.DataFrame(filtered_data, columns=['Content', 'Date'])
+    df.to_csv('hasil.csv', encoding='UTF-8', index=False)
 
 
 def to_gmt7(twitter_date):
